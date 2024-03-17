488033b071e3e4f6d352782467eec28f264aef75
Ali Zaidi <162324371+Aliasimshahmashwani677@users.noreply.github.com>
2024-03-17 11:27:29 +0500

    versioning  importance

---
+++ Versioning node-js/index.js
@@ -0,0 +1,16 @@
+
+
+
+
+//version 
+/*4.18.3
+3=>(3) minor fix
+2=>(18)major bug fix
+1=>(4) major release
+|^4.18.3 compatible with  all  version
+until 5 version
+~ approximately
+
+dont use  latest version  may have some issues
+*/
+
