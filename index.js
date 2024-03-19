3eeb4ea8aa17c5f1d217bc8392abe11a93a88e64
Ali Zaidi <162324371+Aliasimshahmashwani677@users.noreply.github.com>
2024-03-19 22:58:56 +0500

    JS-Arrays

--- index.js
+++ index.js
@@ -1,21 +1,102 @@
-const http = require("http");
-const fs = require('fs');
-const myServer= http.createServer((req , res) =>{
-    const log = `${Date.now()}: ${req.url} new req received\n`;
-
-    fs.appendFile("log.txt",log ,(err, data) => {
-        switch(req.url){
-            case '/':res.end("HOMEPAGE");
-            break;
-            case'/about': res.end("i am asim ali");
-            break;
-            default:
-                res.end("404 Not Found");
-        }
-    res.end("hello from server again");
-    });
-
-});
-myServer.listen(8000,() => console.log("server started!"));
+//const shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+//shopping[2]="asim";
+//console.log(shopping);
 
 
+//console.log(shopping.length);
+//console.log(shopping[]);
+
+//const sequence = [1, 1, 2, 3, 5, 8, 13];
+//const random = ["tree", 795, [0, 1, 2]];
+//const randomArray=["asim",2,0.5,["true"]];
+//randomArray[0][0];
+
+//const random = ["tree", 795, [0, 1, 2]];
+/*const birds = ["Parrot", "Falcon", "Owl"];
+console.log(birds.indexOf("Owl")); //  2
+console.log(birds.indexOf("Rabbit")); // -1
+//console.log (random[0][2]);
+
+
+const cities = ["Manchester", "Liverpool"];
+cities.push("Cardiff");
+console.log(cities); // [ "Manchester", "Liverpool", "Cardiff" ]
+cities.push("Bradford", "Brighton");
+console.log(cities); // [ "Manchester", "Liverpool", "Cardiff", "Bradford", "Brighton" ]
+cities.push("ghazi", "swabi");
+console.log(cities);
+*/
+
+//const cities = ["Manchester", "Liverpool"];
+
+//const newLength = cities.push("haripur","bristol");
+//console.log(newLength);
+
+
+
+/* const cities = ["Manchester", "Liverpool"];
+cities.unshift("aliabad");
+console.log(cities); // [ "Edinburgh", "Manchester", "Liverpool" ] */
+
+//const cities = ["Manchester", "Liverpool"];
+//const removedCity = cities.pop();
+//console.log(removedCity); // "Liverpool"  
+
+
+//const cities = [   "Manchester", "Liverpool"];
+//cities.pop();
+//console.log(cities); // [ "Manchester" ]
+
+
+//const cities = ["Manchester", "Liverpool",];
+/*  const cities = ["Manchester", "Liverpool", "Edinburgh", "Carlisle"];
+const index = cities.indexOf("Liverpool");
+if (index !== -1) {
+  cities.splice(index, 1);
+}
+console.log(cities); // [ "Manchester", "Edinburgh", "Carlisle" ]
+
+//console.log(cities); // [ "Liverpool" ] */
+/*
+const cities =["ghazi", "haripur"];
+const index = cities.indexOf("haripur");
+
+if(index !== -1) {
+
+    cities.splice(index ,1);
+} */
+
+
+/*const cities = ["Manchester", "Liverpool", "Edinburgh", "Carlisle"];
+const index = cities.indexOf("Liverpool");
+if (index !== -1) {
+  cities.splice(index, 2);
+}
+console.log(cities);  */ // [ "Manchester", "Carlisle" ]
+
+
+//const birds = ["parrot", "owl"];
+//for (const bird of birds){
+ //   console.log(bird);
+//}
+
+// map func ;
+/*    function d(number) {
+  return number * 2;
+}
+const numbers = [5, 2, 7, 6];
+const dd = numbers.map(d);
+console.log(dd); // [ 10, 4, 14, 12 ]
+*/
+
+/*function isLonger(city){
+  return city .length > 8;
+}
+
+const cities = ["edinburgg", " NORD DE Lille"];
+
+longer = cities.filter(isLonger);
+
+console.log (longer);
+
+*/
